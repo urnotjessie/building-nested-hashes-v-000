35c6480eda0589428f77fc8558952b68691e99e1
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 02:10:54 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,9 +18,11 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        name: "Romeo", age: "15", status: "alive"
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
         #your key/value pairs here
