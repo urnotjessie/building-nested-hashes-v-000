95d31c13285577f0b722324dcbcaea43b8b94d89
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 02:16:41 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here:
   # Use the above method to change the status of our hero Romeo and our heroine Juliet from "alive" to "dead".
-
+  epic_tragedy[:montague][:hero][:status] = "dead"
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
