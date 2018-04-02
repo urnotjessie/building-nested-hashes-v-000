04a0ba135b5d960d61236976b40c5d52223f5f72
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 02:10:25 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,6 +7,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lord Montague", age: "53"
         },
       :hero => {
         #your key/value pairs here
